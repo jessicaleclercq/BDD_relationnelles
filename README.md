46f4a2d2a1a18cc88958011da5c4c3e152f18638
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-04-30 22:48:35 +0200

    ajoutdocto

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+##Le futur Doctolib 
+
+Cette application permet de regrouper dans une base de données des docteurs et patients, ainsi que de répertorier leurs RDV et leur ville. 
---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+
+
+100.times do
+  city = City.create!(
+    name: Faker::Address.city,
+  )
+  doctor = Doctor.create!(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    specialty: ["généraliste","gynécologue", "dentiste", "homéopathe","psychologue","psychiatre"].sample, 
+    zip_code: Faker::Address.zip_code
+    )
+  patient = Patient.create!(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name
+    )
+  appointment = Appointment.create!(
+    date: Faker::Date.forward(days: 30), 
+    doctor: doctor, 
+    patient: patient
+    )
+
+end
